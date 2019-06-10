a6a4d40d14d163e7fcb84b7f92ee4df7d2df0a64
[leaf97] <[leafidentity7@gamil.com]>
2019-06-10 23:55:30 +0900

    ギターのコード追加

--- MyMusic/ViewController.swift
+++ MyMusic/ViewController.swift
@@ -17,8 +17,7 @@ class ViewController: UIViewController {
     }
 
 // シンバルの音源ファイルを指定
-let cymbalPath =
-    Bundle.main.bundleURL.appendingPathComponent("cymbal.mp3")
+let cymbalPath = Bundle.main.bundleURL.appendingPathComponent("cymbal.mp3")
     
 //シンバル用のプレイヤーインスタンスを作成
 var cymbalPlayer = AVAudioPlayer()
@@ -27,15 +26,35 @@ var cymbalPlayer = AVAudioPlayer()
 
 do{
 //シンバル用のプレイヤーに、音源ファイルを指定
-cymbalPlayer = try AVAudioPlayer(contentsOf: cymbalPath, fileTypeHint: nil)
+cymbalPlayer = try AVAudioPlayer(contentsOf: cymbalPath , fileTypeHint: nil)
         
 //シンバルの音の再生
 cymbalPlayer.play()
 } catch {
   print("シンバルでエラーが発生しました！")
         }
+    }
     
+//ギターの音源ファイルを指定
+let guitarPath = Bundle.main.bundleURL.appendingPathComponent("guitar.mp3")
+        
+//ギター用のプレイヤーインスタンスを作成
+var guitarPlayer = AVAudioPlayer()
+        
+    
+    @IBAction func guitar(_ sender: Any) {
+        
+        do{
+// ギター用のプレイヤーに、音源ファイルを指定
+guitarPlayer = try AVAudioPlayer(contentsOf: guitarPath , fileTypeHint: nil)
+            
+//ギターの音の再生
+guitarPlayer.play()
+    }catch{
+ print("ギターでエラーが発生ました！")
+        }
+    }
 
-}
+    
 }
 
