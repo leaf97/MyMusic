e1074b02b9bb767a5d108342df37c8b735c54f71
[leaf97] <[leafidentity7@gamil.com]>
2019-06-10 21:44:22 +0900

    Initial Commit

---
+++ MyMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by 児玉楓実 on 2019/06/10.
+//  Copyright © 2019 KodamaFumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
